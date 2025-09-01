231123b71f5822dc5f5f4cdf172a96590efb61e0
Shaun Bresnahan <ashaun@ukho.gov.uk>
2025-09-01 11:40:59 +0100

    test refactor

--- main.tf
+++ main.tf
@@ -2,64 +2,159 @@ locals {
   vnet_name = "${var.id}-vnet"
 }
 
+# Spoke RG
 data "azurerm_resource_group" "main" {
   provider = azurerm.spoke
   name     = var.spokerg
 }
 
+# Spoke Route Table
 resource "azurerm_route_table" "main" {
   provider            = azurerm.spoke
   name                = var.routetable
   location            = data.azurerm_resource_group.main.location
   resource_group_name = data.azurerm_resource_group.main.name
 
-  lifecycle { ignore_changes = [tags] }
+  lifecycle {
+    ignore_changes = [tags]
+  }
 }
 
+# Subnets (for_each instead of count)
 data "azurerm_subnet" "main" {
-  count                = length(var.subnets)
+  for_each             = toset(var.subnets)
   provider             = azurerm.spoke
-  name                 = var.subnets[count.index].name
+  name                 = each.value
   resource_group_name  = var.spokerg
   virtual_network_name = local.vnet_name
 }
 
+# Subnet → Route Table associations
 resource "azurerm_subnet_route_table_association" "main" {
-  count          = length(var.subnets)
+  for_each       = data.azurerm_subnet.main
   provider       = azurerm.spoke
-  subnet_id      = data.azurerm_subnet.main[count.index].id
+  subnet_id      = each.value.id
   route_table_id = azurerm_route_table.main.id
 }
 
+# Hub RG
 data "azurerm_resource_group" "hub" {
   provider = azurerm.hub
   name     = var.hubrg
 }
 
+# Hub Route Table
 data "azurerm_route_table" "main" {
   provider            = azurerm.hub
   name                = var.hubrt
   resource_group_name = data.azurerm_resource_group.hub.name
 }
 
-resource "azurerm_route" "main" {
+# Spoke Routes
+resource "azurerm_route" "spoke" {
+  for_each               = { for r in var.spoke_routes : r.name => r }
   provider               = azurerm.spoke
-  name                   = var.spokeroute[count.index]
+  name                   = each.value.name
   resource_group_name    = data.azurerm_resource_group.main.name
   route_table_name       = azurerm_route_table.main.name
-  address_prefix         = var.spokeprefix[count.index]
-  next_hop_type          = var.hop[count.index]
-  next_hop_in_ip_address = var.nexthopipaddress[count.index]
-  count                  = length(var.spokeroute)
+  address_prefix         = each.value.address_prefix
+  next_hop_type          = each.value.next_hop_type
+  next_hop_in_ip_address = lookup(each.value, "next_hop_in_ip_address", null)
 }
 
-resource "azurerm_route" "main1" {
+# Hub Routes
+resource "azurerm_route" "hub" {
+  for_each               = { for r in var.hub_routes : r.name => r }
   provider               = azurerm.hub
-  name                   = var.hubroute[count.index]
+  name                   = each.value.name
   resource_group_name    = data.azurerm_resource_group.hub.name
   route_table_name       = data.azurerm_route_table.main.name
-  address_prefix         = var.hubprefix[count.index]
-  next_hop_type          = var.hop[count.index]
-  next_hop_in_ip_address = var.nexthopipaddress[count.index]
-  count                  = length(var.hubroute)
+  address_prefix         = each.value.address_prefix
+  next_hop_type          = each.value.next_hop_type
+  next_hop_in_ip_address = lookup(each.value, "next_hop_in_ip_address", null)
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#locals {
+#  vnet_name = "${var.id}-vnet"
+#}
+#
+#data "azurerm_resource_group" "main" {
+#  provider = azurerm.spoke
+#  name     = var.spokerg
+#}
+#
+#resource "azurerm_route_table" "main" {
+#  provider            = azurerm.spoke
+#  name                = var.routetable
+#  location            = data.azurerm_resource_group.main.location
+#  resource_group_name = data.azurerm_resource_group.main.name
+#
+#  lifecycle { ignore_changes = [tags] }
+#}
+#
+#data "azurerm_subnet" "main" {
+#  count                = length(var.subnets)
+#  provider             = azurerm.spoke
+#  name                 = var.subnets[count.index].name
+#  resource_group_name  = var.spokerg
+#  virtual_network_name = local.vnet_name
+#}
+#
+#resource "azurerm_subnet_route_table_association" "main" {
+#  count          = length(var.subnets)
+#  provider       = azurerm.spoke
+#  subnet_id      = data.azurerm_subnet.main[count.index].id
+#  route_table_id = azurerm_route_table.main.id
+#}
+#
+#data "azurerm_resource_group" "hub" {
+#  provider = azurerm.hub
+#  name     = var.hubrg
+#}
+#
+#data "azurerm_route_table" "main" {
+#  provider            = azurerm.hub
+#  name                = var.hubrt
+#  resource_group_name = data.azurerm_resource_group.hub.name
+#}
+#
+#resource "azurerm_route" "main" {
+#  provider               = azurerm.spoke
+#  name                   = var.spokeroute[count.index]
+#  resource_group_name    = data.azurerm_resource_group.main.name
+#  route_table_name       = azurerm_route_table.main.name
+#  address_prefix         = var.spokeprefix[count.index]
+#  next_hop_type          = var.hop[count.index]
+#  next_hop_in_ip_address = var.nexthopipaddress[count.index]
+#  count                  = length(var.spokeroute)
+#}
+#
+#resource "azurerm_route" "main1" {
+#  provider               = azurerm.hub
+#  name                   = var.hubroute[count.index]
+#  resource_group_name    = data.azurerm_resource_group.hub.name
+#  route_table_name       = data.azurerm_route_table.main.name
+#  address_prefix         = var.hubprefix[count.index]
+#  next_hop_type          = var.hop[count.index]
+#  next_hop_in_ip_address = var.nexthopipaddress[count.index]
+#  count                  = length(var.hubroute)
+#}
--- variables.tf
+++ variables.tf
@@ -1,36 +1,83 @@
 variable "spokerg" {
- description = "name of spoke resource group"
+  description = "Name of spoke resource group"
 }
+
 variable "hubrg" {
- description = "name of hub resource group"
+  description = "Name of hub resource group"
 }
+
 variable "hubrt" {
-  description = "hub route table name" 
+  description = "Hub route table name"
 }
+
 variable "id" {
-  description = "environment you're deploying too"
+  description = "Environment you're deploying to"
 }
+
 variable "routetable" {
-  description = "spoke route table"
+  description = "Spoke route table"
 }
-variable "spokeroute" {
-  description = "Spoke routetable route array"
-}
-variable "hubroute" {
-  description = "Hub routetable routes array"
-}
-variable "hop" {
-  description = "The type of hop you require in a array"
-}
-variable "nexthopipaddress" {
-  description = "The next hop IP address in a array, this is only required if the next hop type is set to VirtualAppliance"
-}
+
 variable "subnets" {
- description = "array contains names of subnets, the subnet array used on the tfmodule-azure-vnet-with-nsg fits this expected pattern" 
+  description = "List of subnet names"
+  type        = list(string)
 }
-variable "spokeprefix" {
-  description = "Spoke ip route array"
+
+variable "spoke_routes" {
+  description = "List of spoke routes"
+  type = list(object({
+    name                   = string
+    address_prefix         = string
+    next_hop_type          = string
+    next_hop_in_ip_address = optional(string)
+  }))
 }
-variable "hubprefix" {
-  description = "Hub ip route array"
+
+variable "hub_routes" {
+  description = "List of hub routes"
+  type = list(object({
+    name                   = string
+    address_prefix         = string
+    next_hop_type          = string
+    next_hop_in_ip_address = optional(string)
+  }))
 }
+
+
+
+#variable "spokerg" {
+# description = "name of spoke resource group"
+#}
+#variable "hubrg" {
+# description = "name of hub resource group"
+#}
+#variable "hubrt" {
+#  description = "hub route table name" 
+#}
+#variable "id" {
+#  description = "environment you're deploying too"
+#}
+#variable "routetable" {
+#  description = "spoke route table"
+#}
+#variable "spokeroute" {
+#  description = "Spoke routetable route array"
+#}
+#variable "hubroute" {
+#  description = "Hub routetable routes array"
+#}
+#variable "hop" {
+#  description = "The type of hop you require in a array"
+#}
+#variable "nexthopipaddress" {
+#  description = "The next hop IP address in a array, this is only required if the next hop type is set to VirtualAppliance"
+#}
+#variable "subnets" {
+# description = "array contains names of subnets, the subnet array used on the tfmodule-azure-vnet-with-nsg fits this expected pattern" 
+#}
+#variable "spokeprefix" {
+#  description = "Spoke ip route array"
+#}
+#variable "hubprefix" {
+#  description = "Hub ip route array"
+#}

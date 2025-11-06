8573a10c736d5de98394d2ab0272882b3ff3e0a2
Mark <52573122+skins-uk@users.noreply.github.com>
2025-11-06 13:28:00 +0000

    allows for mix of hop types (#11)

--- route-table.tf
+++ route-table.tf
@@ -24,20 +24,19 @@ resource "azurerm_subnet_route_table_association" "main" {
 resource "azurerm_route" "spoke" {
   for_each = {
     for idx, route_name in var.spokeroute : route_name => {
-      name           = route_name
-      address_prefix = var.spokeprefix[idx]
-      next_hop_type  = var.hop[idx]
-      # Include next_hop_ip_address only if the next hop type is VirtualAppliance
-      next_hop_in_ip_address = var.nexthopipaddress[idx]
+      name                   = route_name
+      address_prefix         = var.spokeprefix[idx]
+      next_hop_type          = var.hop[idx]
+      next_hop_in_ip_address = var.hop[idx] == "VirtualAppliance" ? var.nexthopipaddress[idx] : null
     }
   }
 
-  provider            = azurerm.spoke
-  name                = each.value.name
-  resource_group_name = data.azurerm_resource_group.main.name
-  route_table_name    = azurerm_route_table.main.name
-  address_prefix      = each.value.address_prefix
-  next_hop_type       = each.value.next_hop_type
+  provider               = azurerm.spoke
+  name                   = each.value.name
+  resource_group_name    = data.azurerm_resource_group.main.name
+  route_table_name       = azurerm_route_table.main.name
+  address_prefix         = each.value.address_prefix
+  next_hop_type          = each.value.next_hop_type
   next_hop_in_ip_address = each.value.next_hop_in_ip_address
 } 
 
